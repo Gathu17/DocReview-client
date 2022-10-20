7ff6721bc4bbd498d8847a14a296489d85f73192
jayGathu <jjgathu17@gmail.com>
2022-10-20 13:45:10 +0300

    fixed corrupt file

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import File from './Pages/Files/file'
 
 function App() {
   return (
-    <BrowserRouter basename="/" >
+    <BrowserRouter >
     <NavBar/>
     <Routes>
       <Route path="/"  element={<Home/>}/>
